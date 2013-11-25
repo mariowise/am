04f73bc901c9b8fdbf67e3dfa408d63efc03c21c
Mario López <mariolopezlandes@gmail.com>
2013-11-25 12:02:27 -0300

    AssetsController (10%)

--- application/models/Asset.php
+++ application/models/Asset.php
@@ -1,6 +1,6 @@
 <?php
 
-class Application_Model_Asignacion extends Zend_Db_Table_Abstract
+class Application_Model_Asset extends Zend_Db_Table_Abstract
 {
 	protected $_name = 'ASSET';
 	protected $_primary = 'ID';
